0d96abaabcacd09075a102c13c7b61a042b748cc
lbzuluagag <35717619+lbzuluagag@users.noreply.github.com>
2020-06-04 20:05:58 -0500

    furier actualizado, genera cancion completa.

--- fourier.py
+++ fourier.py
@@ -1,29 +1,26 @@
 import scipy.io.wavfile as wavfile
 import numpy as np
-import matplotlib.pyplot as plt
-plt.rcParams['figure.figsize'] = [16,12]
-plt.rcParams.update({'font.size' : 18})
 
 
 def main():
-    s_rate, signal =wavfile.read('marconi-master/songs/bad_songs/hakuna_rosa.wav')
-    sample_badRate, badSample = wavfile.read('marconi-master/songs/bad_songs/hakuna_rosa.wav')
+    s_rate, signal =wavfile.read('marconi-master/songs/bad_songs/bad_song.wav')
+    sample_badRate, badSample = wavfile.read('marconi-master/songs/bad_songs/bad_song.wav')
     sample_rate, samples = wavfile.read('marconi-master/songs/hakuna_matata.wav')
     producto =[]
 
     dt = 0.001
-    t = np.arange(0,10,dt)
+    t = np.arange(0,3,dt)
     print(len(badSample))
-    for a in range(0,int(len(badSample)-10000),10000):
-        f = badSample[a:a+10000]
+    for a in range(0,int(len(badSample)-3000),3000):
+        f = badSample[a:a+3000]
         n = len(t)
         fhat = np.fft.fft(f,n)
         psd = fhat * np.conj(fhat) / n
         freq = (1/(dt*n)) * np.arange(n)
-        indices = psd >300000000
+        indices = psd >150000000
         psdclean = psd * indices
         fhat = indices * fhat
-        ffilt = (np.fft.ifft(fhat))/30000
+        ffilt = (np.fft.ifft(fhat))/10000
         producto = np.append(producto,ffilt.real)
 
     wavfile.write("furier.wav",sample_badRate,producto)
